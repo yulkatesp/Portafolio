0ee5eddf1aef3191b17c8d049265e08493cc92b7
Katerin Espitia <katherineee12365@gmail.com>
2026-04-27 21:39:17 -0500

    Modificando image

--- app.py
+++ app.py
@@ -41,6 +41,6 @@ import streamlit.components.v1 as components
 
 components.html(
     html_content,
-    height=500,          # Ajusta según la altura de tu diseño
+    height=600,          # Ajusta según la altura de tu diseño
     scrolling=False,
 )
